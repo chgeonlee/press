f2c0ace1be2a268e79f6494b93508326fc4b4062
chunggeonlee <chgeon.lee@aqara.kr>
2023-11-16 10:30:17 +0900

    debug

--- app/face/src/routes/Device.tsx
+++ app/face/src/routes/Device.tsx
@@ -2,6 +2,9 @@ import _ from "lodash";
 
 const Device = () => {
   //window.screen
+  //@ts-ignore
+  const n = window.screen.mozOrientation ? "moz..." : "no";
+
   return (
     <div>
       <div> userAgent: {window.navigator.userAgent}</div>
@@ -12,6 +15,7 @@ const Device = () => {
             ? window.screen.orientation.type
             : "오리엔테이션이 존재하지 않음"
           : "screen이 존재하지 않음"}
+        {n}
       </div>
     </div>
   );
