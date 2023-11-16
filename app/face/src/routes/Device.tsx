8d386f3ea9fbaffff304c630b9ec0d3182f44b2c
chunggeonlee <chgeon.lee@aqara.kr>
2023-11-16 10:28:09 +0900

    debug

--- app/face/src/routes/Device.tsx
+++ app/face/src/routes/Device.tsx
@@ -1,5 +1,20 @@
+import _ from "lodash";
+
 const Device = () => {
-  return <div>{window.navigator.userAgent}</div>;
+  //window.screen
+  return (
+    <div>
+      <div> userAgent: {window.navigator.userAgent}</div>
+      <div>
+        window.screen:{" "}
+        {window.screen
+          ? window.screen.orientation
+            ? window.screen.orientation.type
+            : "오리엔테이션이 존재하지 않음"
+          : "screen이 존재하지 않음"}
+      </div>
+    </div>
+  );
 };
 
 export default Device;
